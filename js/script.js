c0ef4a014eba154eac9291c677f3054bb425b2ff
serhii-sultanov <sshsultanov@gmail.com>
2022-08-24 14:30:39 +0300

    added promise

--- js/script.js
+++ js/script.js
@@ -1,1 +1,29 @@
 "use strict";
+
+// 1
+const promise = new Promise(function (resolve, reject) {
+  setTimeout(() => resolve("done"), 5000);
+});
+
+promise
+  .then((result) => {
+    console.log(result);
+  })
+  .catch(() => {
+    throw new Error("error");
+  })
+  .finally(() => {
+    console.log("Promise was finished!");
+  });
+
+//   2
+try {
+  const promise = new Promise(function (resolve, reject) {
+    setTimeout(() => resolve("done"), 5000);
+  });
+  console.log("done");
+} catch {
+  throw new Error("error");
+} finally {
+  console.log("Promise was finished!");
+}
